655477171e14f496716f9554cb431dd1664ed3f7
Abdo7126 <154598923+Abdo7126@users.noreply.github.com>
2026-07-01 09:43:30 +0300

    Add files via upload

--- assets/softwarezawy-sync-config.js
+++ assets/softwarezawy-sync-config.js
@@ -1,6 +1,6 @@
 window.SOFTWAREZAWY_CLOUD_SYNC = {
   enabled: true,
-  endpoint: "https://script.google.com/macros/s/AKfycbxsTVsZy37m25D5iHH1WZiua_AvQOjk1RLv7AcSQ4BMYD-TtmhHAGuvUBOtMlka-xK0/exec",
+  endpoint: "https://script.google.com/macros/s/AKfycbw9xiy_Cfg87gobxzkmbiAAqxxFMzlLEfZN-TcAt3gS_8ekejTzb9jgJ8_92jvzHBJtuw/exec",
   timeoutMs: 4500,
   autoPull: true,
   pullIntervalMs: 60000,
